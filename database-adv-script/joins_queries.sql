9ebac125d39e9cd344340fc62163eb9d4113b935
SPaci20 <spaci20@gmail.com>
2025-07-12 05:16:49 -0700

    Added ORDER BY

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -20,7 +20,8 @@ SELECT
     reviews.rating,
     reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+LEFT JOIN reviews ON properties.property_id = reviews.property_id
+ORDER BY properties.property_id, reviews.rating DESC;
 
 
 -- 3️⃣ FULL OUTER JOIN: Retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user
